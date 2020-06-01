b88fec96673ff63e7f8c575c0a0236cd8b92c722
Alex Romaniuk <alaster.rom@gmail.com>
2020-06-01 11:59:01 +0300

    add 1 file java / plus change file metod

---
+++ AddTwoNumbers.java
@@ -0,0 +1,13 @@
+package src;
+
+public class AddTwoNumbers {
+   public static void main(String[] args) {
+      int num1 = 4;
+      int num2 = 16;
+      int del;
+      del = num1 / num2;
+      
+      logger.log("Sum of these numbers: " + del);
+   }
+
+}
