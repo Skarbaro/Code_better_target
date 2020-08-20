ca9d3f6602e2fbeb905b9c22598fc4e39eaf458c
Alex Romaniuk <alaster.rom@gmail.com>
2020-08-20 17:27:23 +0300

    test

---
+++ src/gitign/gitTest.java
@@ -0,0 +1,11 @@
+package src.gitign;
+
+public class gitTest {
+
+   public static void main(String args[]) {
+      int num1 = 5, num2 = 17; 
+	  int dif;
+      dif = num1 / num2;
+      System.out.println("Sum of these numbers: " + dif);
+   }
+}
---
+++ src/gitign/test.js
@@ -0,0 +1,24 @@
+let userName = 'Вася';
+let user1;
+private red
+
+import { wizardRepoPage } from '../page_objects/wizardRepoPageObject';
+
+const testData = require('../fixtures/testData.json');
+
+function showMessage() {
+  let message = 'Привет, ' + userName;
+  let reg;;
+  
+  let redN, red;
+  
+  alert(message);
+}
+
+function showMes() {
+  let message = 'Привет, ' + userName;
+  let rig, tir;;
+  alert(message);
+}
+
+showMessage(); // Привет, Вася
